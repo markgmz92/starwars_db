5e47e66c69b61dda5eb1b32c0f18ca72b1f4ad6b
markgmz92 <markgmz92@gmail.com>
2021-01-27 01:57:52 +0800

    [SW - 003] Used img for background

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Landing.module.css";
+
+const Landing = () => {
+  return (
+    <div className={styles.landing}>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Landing;
